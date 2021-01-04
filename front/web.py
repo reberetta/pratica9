14cfa867a4a6ac2ef65499afd1d2502fb8513ef2
Regina Beretta <reginabmre@gmail.com>
2021-01-04 14:50:52 -0400

    inicio mkp

---
+++ front/web.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+from back.marketplace import add_new_marketplace
+
+import sys  
+sys.path.append('pratica9/')
+
+
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+titulo_app = 'Pratica 9'
+
+@app.route('/')
+def index():
+
+    return render_template('index.html', nome=titulo_app )
+
+@app.route('/addmkp')
+def addmkp():  
+
+    return render_template('addmkp.html', nome=titulo_app )
+
+@app.route('/sucesso')
+def sucesso():  
+
+    mkp = request.args.get('mkp')
+
+
+    return f'Sucesso' 
+
+
+app.run(debug=True) 
